25985331c5cec8b6dda81907fb594416d56626b3
Divneet Singh <divneet100singh@gmail.com>
2020-07-23 02:29:47 +0530

    port changes for application deployment.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
-const app = express();
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 // absolute path to the public assets.
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -90,6 +92,6 @@ app.get("*", (req, res) => {
     })
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is up on port 3000");
 });
